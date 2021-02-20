47b772a33153e62bba6b1ec6c31c646733b31667
Asano-Ryota <73601385+Asano-Ryota@users.noreply.github.com>
2021-02-20 22:25:47 +0900

    ボタンの修正

--- countdown.js
+++ countdown.js
@@ -1,4 +1,4 @@
-var endDate = new Date('2021/03/01 00:00:00');
+var endDate = new Date('2021/03/08 12:00:00');
 var interval = 1000;
 
 
@@ -17,9 +17,9 @@ function countdownTimer(){
         period -= (minutes * (1000 * 60));
         var second = Math.floor(period / 1000);
         var insert = "";
-        insert += '<span class="h">' + addZeroDay(day) +'<span class="text-3xl">日</span>' + '</span><br class="md:hidden">';
-        insert += '<span class="h">' + addZero(hour) + '<span class="text-3xl">時間'+'</span>';
-        insert +=  '<span class="m">' + addZero(minutes) +'<span class="text-3xl">分' + '</span>';
+        insert += '<span class="h">' + day +'<span class="text-3xl">日</span>' + '</span><br class="md:hidden">';
+        insert += '<span class="h">' + hour + '<span class="text-3xl">時間'+'</span>';
+        insert +=  '<span class="m">' + minutes +'<span class="text-3xl">分' + '</span>';
         insert += '<span class="s">' + addZero(second)+ '<span class="text-3xl">秒'+ '</span>';
         document.getElementById('result').innerHTML = insert;
         setTimeout(countdownTimer,10);
